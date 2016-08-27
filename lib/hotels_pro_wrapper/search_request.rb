4d1eed6e39d06c92255fc3b8e9d80ec46237628b
tmbv93 <tmbv93@gmail.com>
2016-08-27 18:39:57 +0200

    Currency is configurable

--- lib/hotels_pro_wrapper/search_request.rb
+++ lib/hotels_pro_wrapper/search_request.rb
@@ -8,16 +8,17 @@ module HotelsProWrapper
       self.class.base_uri HotelsProWrapper.api_base
       self.class.basic_auth(HotelsProWrapper.configuration.username, HotelsProWrapper.configuration.password)
       @options = {query: {
-          currency: 'USD',
+          currency: HotelsProWrapper.configuration.currency,
           pax: search_args[:pax],
           checkin: search_args[:checkin],
           checkout: search_args[:checkout],
           destination_code: HotelsProWrapper.configuration.destination_code,
-          client_nationality: 'NO',
-          hotel_codes: '102165, 1021eb, 1022ce',
-          max_product: 1,
-          limit: 10
+          client_nationality: 'NO'
         }}
+
+      @options[:query][:hotel_code] = search_args[:hotel_code] if search_args.has_key? :hotel_code
+      @options[:query][:max_product] = search_args[:max_product] if search_args.has_key? :max_product
+      @options
     end
 
     def response
